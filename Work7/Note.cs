6cfa485970bc8a4da65547c6f3a68f5a62e4f674
zaretliz <liza_zar_06@mail.ru>
2021-11-23 21:06:10 +0500

    Hw7

---
+++ Work7/Note.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work7
+{
+    struct Note
+    {
+        #region Свойства
+        /// <summary>
+        /// Номер записи
+        /// </summary>
+        public int ID { get { return this.id; } set { this.id = value; } }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime Data { get { return this.data; } set { this.data = value; } }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get { return this.name; } set { this.name = value; } }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int Age { get { return this.age; } set { this.age = value; } }
+
+        /// <summary>
+        /// Рост
+        /// </summary>
+        public int Height { get { return this.height; } set { this.height = value; } }
+
+        /// <summary>
+        /// Вес
+        /// </summary>
+        public int Weight { get { return this.weight; } set { this.weight = value; } }
+        #endregion
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="data"></param>
+        /// <param name="Name"></param>
+        /// <param name="age"></param>
+        /// <param name="height"></param>
+        /// <param name="weight"></param>
+        public Note(int ID, DateTime Data, string Name, int Age, int Height, int Weight)
+        {
+            this.id = ID;
+            this.data = Data;
+            this.name = Name;
+            this.age =Age;
+            this.height = Height;
+            this.weight = Weight;
+        }
+
+        /// <summary>
+        /// Вывод данных в строке
+        /// </summary>
+        /// <returns></returns>
+        public string Print()
+        {
+            return $"{this.ID,10} {this.Data,25} {this.Name,10} {this.Age,10} {this.Height,17} {this.Weight,10}";
+        }
+
+        #region Поля
+        /// <summary>
+        /// Поле "ID"
+        /// </summary>
+        private int id;
+
+        /// <summary>
+        /// Поле "Дата"
+        /// </summary>
+        private DateTime data;
+
+        /// <summary>
+        /// Поле "Имя"
+        /// </summary>
+        private string name;
+
+        /// <summary>
+        /// Поле "Возраст"
+        /// </summary>
+        private int age;
+
+        /// <summary>
+        /// Поле "Рост"
+        /// </summary>
+        private int height;
+
+        /// <summary>
+        /// Поле "Вес"
+        /// </summary>
+        private int weight;
+        #endregion
+    }
+}
---
+++ Work7/Repository.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work7
+{
+    /// <summary>
+    /// Репозиторий
+    /// </summary>
+    struct Repository
+    {
+        /// <summary>
+        /// Массив заголовков в файле
+        /// </summary>
+        string[] titles;
+
+        /// <summary>
+        /// Массив для ежедневника
+        /// </summary>
+        private Note[] note;
+
+        /// <summary>
+        ///  Путь к файлу 
+        /// </summary>
+        private string path;
+
+        /// <summary>
+        /// Индекс элемента в массиве
+        /// </summary>
+        int index;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="Path">Путь в файлу с данными</param>
+        public Repository(string Path)
+        {
+            this.path = Path;
+            this.index = 0;
+            this.titles = new string[0];
+            this.note = new Note[1];
+
+            this.Load();
+        }
+
+        /// <summary>
+        /// Метод увеличения текущего хранилища
+        /// </summary>
+        /// <param name="Flag">Условие увеличения</param>
+        private void Resize(bool Flag)
+        {
+            if (Flag)
+            {
+                Array.Resize(ref this.note, this.note.Length * 2);
+            }
+        }
+
+        /// <summary>
+        /// Метод записи данных в хранилище
+        /// </summary>
+        /// <param name="NewNote">Новая запись</param>
+        public void Add(Note NewNote)
+        {
+            this.Resize(index >= this.note.Length);
+            this.note[index] = NewNote;
+            this.index++;
+        }
+
+
+        /// <summary>
+        /// Метод загрузки данных
+        /// </summary>
+        private void Load()
+        {
+            using (StreamReader sr = new StreamReader(this.path))
+            {
+                titles = sr.ReadLine().Split(',');
+
+
+                while (!sr.EndOfStream)
+                {
+                    string[] args = sr.ReadLine().Split(',');
+
+                    Add(new Note(Convert.ToInt32(args[0]),
+                                Convert.ToDateTime(args[1]),
+                                args[2],
+                                Convert.ToInt32(args[3]),
+                                Convert.ToInt32(args[4]),
+                                Convert.ToInt32(args[5])));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод сохранения данных
+        /// </summary>
+        /// <param name="Path">Путь к файлу сохранения</param>
+        public void Save(string Path)
+        {
+            string temp = String.Format("{0},{1},{2},{3},{4},{5}",
+                                            this.titles[0],
+                                            this.titles[1],
+                                            this.titles[2],
+                                            this.titles[3],
+                                            this.titles[4],
+                                            this.titles[5]);
+
+            File.AppendAllText(Path, $"{temp}\n");
+
+            for (int i = 0; i < this.index; i++)
+            {
+                temp = String.Format("{0},{1},{2},{3},{4},{5}",
+                                        this.note[i].ID,
+                                        this.note[i].Data,
+                                        this.note[i].Name,
+                                        this.note[i].Age,
+                                        this.note[i].Height,
+                                        this.note[i].Weight);
+                File.AppendAllText(Path, $"{temp}\n");
+            }
+        }
+
+        /// <summary>
+        /// Вывод данных в консоль
+        /// </summary>
+        public void PrintToConsole()
+        {
+            Console.WriteLine($"{this.titles[0],5} {this.titles[1],15} {this.titles[2],15} {this.titles[3],15} {this.titles[4],15} {this.titles[5],10}");
+
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(this.note[i].Print());
+            }
+        }
+
+
+
+    }
+}
